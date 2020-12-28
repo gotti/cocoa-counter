aa79d7f7dd096cc66ccc173435443a2b98b9c262
ExternalExte <ExternalExte@gmail.com>
2020-12-28 15:44:49 +0900

    num->nop #number of people oldnum->oldnop #old number of peope

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ proxy_dict = {
 takoyakisan_apiurl = "YYYYYYYYYYYYYYYY"
 
 scanner = btle.Scanner(0)
-oldnum = 0
-num = 0
+nop = 0 #Number of People
+oldnop = 0 # old Number of People
 lastposted = 0
 lastupdated = 0
 
@@ -69,7 +69,7 @@ while True:
         for (adTypeCode, description, valueText) in dev.getScanData():
             if adTypeCode == 3:
                 device.setuuid(valueText) #  == "0000fd6f-0000-1000-8000-00805f9b34fb"
-                num += 1
+                nop += 1
                 if valueText == "0000fd6f-0000-1000-8000-00805f9b34fb":
                     #print("!----!")
                     print(time.time(),dev.rssi,dev.getScanData())
@@ -121,24 +121,24 @@ while True:
     for k in devicepoplist:
         devices.pop(k)
 
-    num = 0
+    nop = 0
     for k,v in joinedusers.items():
-        num += 1
+        nop += 1
     #print("joined"+str(len(joinedusers)))
     #print("queued"+str(len(queuedusers)))
     if time.time()-lastupdated >= 300:
         lastupdated = time.time()
-        r = requests.get(takoyakisan_apiurl+str(num),proxies=proxy_dict)
+        r = requests.get(takoyakisan_apiurl+str(nop),proxies=proxy_dict)
         if r.json()["status"]!="ok":
             payload1 = {"context:" "APIサーバへのアクセスに失敗"}
             requests.post(posturl, json=payload1,proxies=proxy_dict)
         print("updated")
 
     if time.time()-lastposted >= 300:
-        if len(queuedusers) == 0 and num != oldnum:
+        if len(queuedusers) == 0 and nop != oldnop:
             lastposted = time.time()
-            print(str(num))
+            print(str(nop))
             now = datetime.datetime.fromtimestamp(time.time())
-            payload = {"content": f"[{now.strftime('%H:%M:%S')}] 部室にいるCOCOA利用者:{oldnum}→{num}"}
+            payload = {"content": f"[{now.strftime('%H:%M:%S')}] 部室にいるCOCOA利用者:{oldnop}→{nop}"}
             requests.post(posturl, json=payload, proxies=proxy_dict)
-            oldnum = num
+            oldnop = nop
